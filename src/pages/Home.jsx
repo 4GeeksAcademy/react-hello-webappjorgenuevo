a7183b16f508515f0b89f73faedb01f92922a067
Jorge Sanchez Ortiz <jorgesoibz@gmail.com>
2025-06-26 16:30:44 +0000

    robert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,36 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-
+import {useEffect, useState} from "react";
 export const Home = () => {
+
+
+     
+
 
   const {store, dispatch} =useGlobalReducer()
 
+  useEffect(()=>{
+        fetch("https://playground.4geeks.com/contact/agendas/jorgeSanchez/contacts",{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            },
+     })
+     .then((resp)=>resp.json())
+     .then(data=>{dispatch({type:"get_contact",payload:data.contacts})})
+     },[])
+
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
+		<div>
+            <div>
+                {store.contacts.map((contact,index=>(
+                    <h1 key={index}>{contact.name}</h1>
+                    
+                ))
+
+                )}
+            </div>
+        </div>
 	);
 }; 
--- src/store.js
+++ src/store.js
@@ -1,31 +1,25 @@
 export const initialStore=()=>{
   return{
     message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    contacts:[],
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
+    case "get_contact":
       return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+        ...store,contacts:action.payload
+      }
+      case "delete_contact":
+        let filterContacts = store.contacts.filter(contact=>contact.id!==action.payload)
+        return { ...store,contacts:filterContacts
+
+        }
+        case "add_contact":
+          return {
+            ...store,contacts:[...store.contacts,action.payload]
+          }
     default:
       throw Error('Unknown action.');
   }    
